ec24abcc93f9e5b36aebc265122f9c12c81e881d
Jack Miles <jack.henry.miles@icloud.com>
2021-12-20 17:37:11 +0000

    created oval

--- Week 1/my_drawing.py
+++ Week 1/my_drawing.py
@@ -12,6 +12,12 @@ rect2 = Rectangle()
 rect2.set_width(50)
 rect2.set_height(150)
 rect2.set_color("yellow")
-rect2.draw()
+rect2.set_x(100)
+rect2.set_y(100)
+rect2.draw() 
+
+oval = Oval()
+oval.randomize()
+oval.draw()
 
 paper.display()
